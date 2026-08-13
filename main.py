fe0a6da63ac9ee605c4ca2ea923a64ec2c0d3454
maciek <maciek@wp.pl>
2026-08-13 17:51:01 +0200

    Refactor game logic

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+WINNING_COMBINATIONS = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+
 def display_board(board):
     horizontal_line = '---+---+---'
     print(f'\n {board[0]} | {board[1]} | {board[2]}')
@@ -9,52 +20,41 @@ def display_board(board):
 def make_move(board, player):
     while True:
         position = input(f"Player {player}, choose a position (1-9): ")
-        if position in ['1','2','3','4','5','6','7','8','9']:
-            if board[int(position) - 1] == ' ':
-                board[int(position) - 1] = player
-                return board
+        if position.isdigit() and 1 <= int(position) <= 9:
+            index = int(position) - 1
+            if board[index] == ' ':
+                board[index] = player
+                break
             else:
                 print("That position is already taken!")
         else:
             print("Wrong input, please try again!")
 
 def check_winner(board, player):
-    winning_combinations = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ]
-    for comb in winning_combinations:
-        if board[comb[0]] == player and board[comb[1]] == player and board[comb[2]] == player:
+    for comb in WINNING_COMBINATIONS:
+        if all(board[index] == player for index in comb):
             return True
-
     return False
 
-board = [' ',' ',' ',
-         ' ',' ',' ',
-         ' ',' ',' ']
+def play_game():
 
-print("Welcome to Tic Tac Toe!")
-display_board(board)
-while True:
-    make_move(board, 'X')
+    board = [' ', ' ', ' ',
+             ' ', ' ', ' ',
+             ' ', ' ', ' ']
+
+    print("Welcome to Tic Tac Toe!")
     display_board(board)
-    if check_winner(board, 'X'):
-        print(f'Player X wins!')
-        break
-    elif ' ' not in board:
-        print("It's a draw!")
-        break
-    make_move(board, 'O')
-    display_board(board)
-    if check_winner(board, 'O'):
-        print('Player O wins!')
-        break
-    elif ' ' not in board:
-        print("It's a draw!")
-        break
+    current_player = 'X'
+    while True:
+        make_move(board, current_player)
+        display_board(board)
+        if check_winner(board, current_player):
+            print(f'Player {current_player} wins!')
+            break
+        elif ' ' not in board:
+            print("It's a draw!")
+            break
+        current_player = 'O' if current_player == 'X' else 'X'
+
+if __name__ == '__main__':
+    play_game()
